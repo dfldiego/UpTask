f6ae7fdb56c9e7b3e7437708f899b264645f3135
dfldiego <dfl.diego92@gmail.com>
2020-09-06 14:14:14 -0300

    Permitir a los Usuarios crear sus propias Cuentas. parte4. agregando los usuarios en la base de datos.

--- controllers/usuariosController.js
+++ controllers/usuariosController.js
@@ -1,10 +1,26 @@
-
+const Usuarios = require('../models/Usuarios');
 // GET
 exports.formCrearCuenta = (req, res) => {
     res.render('crearCuenta', {
         nombrePagina: 'Crear Cuenta en UpTask'
     })
 }
+
+// POST
+exports.crearCuenta = (req, res) => {
+    // leer los datos
+    //console.log(req.body);
+    const { email, password } = req.body;
+    //crear el usuario  
+    Usuarios.create({
+        email,
+        password
+    })
+        .then(() => {
+            res.redirect('/iniciar-sesion')
+        });
+}
+
 
 /*
 Tiene que ver con las acciones para crear un usuario/cuenta
